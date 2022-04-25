432d5ebed307cf46ef33c367bc6f808c177f6bd3
ROSH <95622188+roshnirajak@users.noreply.github.com>
2022-04-25 19:29:31 +0530

    updated

--- New folder/folio.js
+++
@@ -1,8 +0,0 @@
-let sky= document.getElementsByClassName('sky');
-let ground= document.getElementsByClassName('ground');
-
-window.addEventListener('scroll', function(){
-    let value= window.scrollY;
-    ground.style.top=value + 'px';
-})
-
